1689d69792d86b9dfe1a7fdd04859f769f8f8fb9
andrej.1998 <andrej.1998@uns.ac.rs>
2019-12-27 18:02:42 +0100

    Dodavanje sortova na listu predmeta

--- src/predmeti/ListaPredmeta.java
+++ src/predmeti/ListaPredmeta.java
@@ -13,8 +13,8 @@ public class ListaPredmeta {
 		this.predmeti = new ArrayList<Predmet>();
 	}
 	
-	public void addPredmet(int sifra, String naziv, int semestar, int godina, Profesor p) {
-		predmeti.add(new Predmet(sifra,naziv,semestar,godina,p));
+	public void addPredmet(int sifra, String naziv, int semestar, int godina, Profesor prof) {
+		predmeti.add(new Predmet(sifra,naziv,semestar,godina,prof));
 	}
 	
 	public void deletePredmet(int sif) {
@@ -37,5 +37,144 @@ public class ListaPredmeta {
 		}
 	}
 	
+	public void sortPredmetiPoSifri(int sort) {
+		if(sort == 0) {  // rastuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getSifraPredmeta() < p.getSifraPredmeta()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+		else {  // opadajuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getSifraPredmeta() > p.getSifraPredmeta()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+	}
+	
+	public void sortPredmetiPoNazivu(int sort) {
+		if(sort == 0) {  // rastuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p.getNaziv().compareTo(p1.getNaziv()) > 0) { // positive value => string1 > string2
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+		else {  // opadajuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p.getNaziv().compareTo(p1.getNaziv()) < 0) { // negative value => string1 < string2
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+	}
+	
+	public void sortPredmetiPoSemestru(int sort) {
+		if(sort == 0) {  // rastuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getSemestar() < p.getSemestar()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+		else {  // opadajuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getSemestar() > p.getSemestar()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+	}
+	
+	public void sortPredmetiPoGodini(int sort) {
+		if(sort == 0) {  // rastuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getGodinaStudija() < p.getGodinaStudija()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+		else {  // opadajuce
+			for(int i = 0; i < predmeti.size() - 1; i++) {
+				Predmet p = predmeti.get(i);
+				for(int j = i + 1; j < predmeti.size(); j++) {
+					Predmet p1 = predmeti.get(j);
+					if(p1.getGodinaStudija() > p.getGodinaStudija()) {
+						Predmet p2 = predmeti.get(i);
+						Predmet p3 = predmeti.get(j);
+						predmeti.set(j, p2);
+						predmeti.set(i, p3);
+						p1 = predmeti.get(j);
+						p = predmeti.get(i);
+					}
+				}
+			}
+		}
+	}
 	
 }
