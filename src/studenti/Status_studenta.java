0b9158ce1c8564890c596722692a87f550bdd882
Repovic_A <aleksarep@uns.ac.rs>
2020-01-04 17:41:31 +0100

    Dodata lista studenata , izmene u enumeraciji sa statusom studenata

--- src/studenti/Status_studenta.java
+++ src/studenti/Status_studenta.java
@@ -5,14 +5,26 @@ public enum Status_studenta{
     B("B"), S("S");
  
     private String status;
- 
+    private int vrednost;
+    
     public String getStatus(){
         return this.status;
     }
  
+    public int getVrednost() {
+    	return this.vrednost;
+    }
+    
+    
     // enum constructor - can not be public or protected
     Status_studenta(String status){
         this.status = status;
+        if ("B".equalsIgnoreCase(status)) {
+			vrednost = 1;
+		}
+        else {
+        	vrednost = 0;
+        }
     }
     
     @Override
