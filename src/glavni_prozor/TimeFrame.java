90adced2750f5e2abe903006e5f00714ffbd0dd3
Repovic_A <aleksarep@uns.ac.rs>
2020-01-04 17:18:08 +0100

    Dodat status bar , menu bar prosiren , mala izmena glavnog prozora

---
+++ src/glavni_prozor/TimeFrame.java
@@ -0,0 +1,38 @@
+package glavni_prozor;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+import java.util.*;
+import java.text.*;
+public class TimeFrame extends JFrame implements ActionListener
+{
+	  JLabel label;
+	  
+	  public TimeFrame()
+	  {
+			SimpleDateFormat sdf = new SimpleDateFormat( "hh:mm dd MMM yyyy" );
+		    label = new JLabel( sdf.format( new GregorianCalendar().getTime() ) );
+		    label.setHorizontalAlignment( JLabel.RIGHT );
+		
+		    Timer timer = new Timer( 1000, this );
+		    timer.start();
+	  }
+	  
+	  public void actionPerformed( ActionEvent ae )
+	  {
+			SimpleDateFormat sdf = new SimpleDateFormat( "hh:mm dd MMM yyyy" );
+		    label.setText( sdf.format( new GregorianCalendar().getTime() ) );
+	  }
+	  
+	public JLabel getLabel() {
+			return label;
+	}
+	
+	public void setLabel(JLabel label) {
+			this.label = label;
+	}
+
+}
--- src/glavni_prozor/menu_bar.java
+++ src/glavni_prozor/menu_bar.java
@@ -1,11 +1,15 @@
 package glavni_prozor;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.KeyStroke;
 
 /*
  * klasa nasleđuje JMenuBar i predstavlja kontejner
@@ -19,37 +23,64 @@ public class menu_bar extends JMenuBar {
 
 	public menu_bar() {
 
+		
+		//dugmici
+		
 		JMenu file = new JMenu("File");
-		JMenu edit = new JMenu("Edit");
-		JMenu help = new JMenu("Help");
-		
-		JMenuItem miFileN = new JMenuItem("New");
-		JMenuItem miFileC = new JMenuItem("Close");
-		
-		file.add(miFileN);
+		JMenuItem miNew = new JMenuItem("New");
+		JMenuItem miClose = new JMenuItem("Close");
+		file.add(miNew);
 		file.addSeparator();
-		file.add(miFileC);
-
-		JMenuItem miEditE = new JMenuItem("Edit");
-		JMenuItem miEditD = new JMenuItem("Delete");
-
-
-		edit.add(miEditE);
+		file.add(miClose);
+		
+		JMenu edit=new JMenu("Edit");
+		JMenuItem edit1=new JMenuItem("Edit");
+		JMenuItem delete=new JMenuItem("Delete");
+		edit.add(edit1);
 		edit.addSeparator();
-		edit.add(miEditD);
+		edit.add(delete);
 		
-		JMenuItem miHelpH = new JMenuItem("Help");
-		JMenuItem miHelpA = new JMenuItem("About");
+		JMenu help=new JMenu("Help");
+		JMenuItem help1=new JMenuItem("Help");
+		JMenuItem about=new JMenuItem("About");
+		help.add(help1);
+		help.add(about);
 		
-		help.add(miHelpH);
-		help.addSeparator();
-		help.add(miHelpA);
 		
-
-
 		add(file);
 		add(edit);
 		add(help);
+		
+
+		//mnemonici i precice
+		
+		file.setMnemonic(KeyEvent.VK_F);
+		
+		
+		
+		miNew.setMnemonic(KeyEvent.VK_N);
+		miNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,ActionEvent.CTRL_MASK));
+		
+		miClose.setMnemonic(KeyEvent.VK_C);
+		miClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,ActionEvent.CTRL_MASK));
+		
+		edit.setMnemonic(KeyEvent.VK_E);
+		
+		edit1.setMnemonic(KeyEvent.VK_E);
+		edit1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E,ActionEvent.CTRL_MASK));
+		
+		delete.setMnemonic(KeyEvent.VK_D);
+		delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D,ActionEvent.CTRL_MASK));
+		
+		help.setMnemonic(KeyEvent.VK_H);
+		
+		help1.setMnemonic(KeyEvent.VK_H);
+		help1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H,ActionEvent.CTRL_MASK));
+		
+		about.setMnemonic(KeyEvent.VK_A);
+		about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,ActionEvent.CTRL_MASK));
+		
+		//ikonice
 
 	}
 
